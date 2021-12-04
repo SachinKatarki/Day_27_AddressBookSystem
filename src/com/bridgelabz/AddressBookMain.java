2366d168875c702670d84c9c7045c1ec8d91220d
Textspot <sachinkatarki1995@gmail.com>
2021-12-04 15:10:16 +0530

    Added: use Console to Edit the Contact in Addressbook in object Oriented Programme

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,11 +1,25 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
-	 public static void main(String[] args) {
+	 static Scanner scanner = new Scanner(System.in);
+
+	    public static void main(String[] args) {
 	        System.out.println("\n Welcome to Address Book System");
 	        AddressBook addressBook = new AddressBook();
-	        addressBook.addContacts();
-	        addressBook.display();
+	        System.out.println("Enter \n1 -> Add Contact \n2 -> Edit Existing Contact ");
+	        switch (scanner.nextInt()) {
+	            case 1:
+	                addressBook.addContacts();
+	                break;
+	            case 2:
+	                addressBook.edit();
+	                break;
+	            default:
+	                System.out.println("Enter valid Number");
+
+	        }
 	    }
 	    }
 
