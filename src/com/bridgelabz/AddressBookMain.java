c07287ba8b7cd0c6dc6c5d8dbe843312390981ba
Textspot <sachinkatarki1995@gmail.com>
2021-12-04 14:54:12 +0530

    Added:Add the New Contact in Addressbook use Console in object Oriented Programme

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -3,6 +3,10 @@ package com.bridgelabz;
 public class AddressBookMain {
 	 public static void main(String[] args) {
 	        System.out.println("\n Welcome to Address Book System");
+	        AddressBook addressBook = new AddressBook();
+	        addressBook.addContacts();
+	        addressBook.display();
+	    }
 	    }
 
-}
+
